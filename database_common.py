8bee48d277c16109d37cb813e8b6475c77c4d473
Gergely Kovacs <gergelyk89@gmail.com>
2018-02-23 10:50:03 +0100

    heroku setup

--- database_common.py
+++ database_common.py
@@ -1,31 +1,20 @@
-import psycopg2
 import psycopg2.extras
 import os
-
-
-def get_connection_string():
-    user_name = os.environ.get('PSQL_USER_NAME')
-    password = os.environ.get('PSQL_PASSWORD')
-    host = os.environ.get('PSQL_HOST')
-    database_name = os.environ.get('PSQL_DB_NAME')
-
-    env_variables_defined = user_name and password and host and database_name
-
-    if env_variables_defined:
-        return 'postgresql://{user_name}:{password}@{host}/{database_name}'.format(
-                                                                                    user_name=user_name,
-                                                                                    password=password,
-                                                                                    host=host,
-                                                                                    database_name=database_name
-                                                                                    )
-    else:
-        raise KeyError('Some necessary environment variable(s) are not defined')
+import psycopg2
+import urllib
 
 
 def open_database():
     try:
-        connection_string = get_connection_string()
-        connection = psycopg2.connect(connection_string)
+        urllib.parse.uses_netloc.append('postgres')
+        url = urllib.parse.urlparse(os.environ.get('DATABASE_URL'))
+        connection = psycopg2.connect(
+            database=url.path[1:],
+            user=url.username,
+            password=url.password,
+            host=url.hostname,
+            port=url.port
+        )
         connection.autocommit = True
     except psycopg2.DatabaseError as exception:
         print('Database connection problem')
@@ -35,10 +24,10 @@ def open_database():
 
 def connection_handler(some_function):
     def wrapper(*args, **kwargs):
-        connection = open_database()
-        dict_cur = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        connection1 = open_database()
+        dict_cur = connection1.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
         ret_value = some_function(dict_cur, *args, **kwargs)
         dict_cur.close()
-        connection.close()
+        connection1.close()
         return ret_value
     return wrapper
